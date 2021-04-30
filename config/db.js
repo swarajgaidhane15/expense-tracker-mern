cab56c0cc5c18cab1986e5b286b6511eebf75316
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2021-04-30 18:46:17 +0530

    trying herkou

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGO_URI = 'YOUR MONGO ATLAS URI here'
+const MONGO_URI =
+  "mongodb+srv://wanda:wanda@cluster0.71dw4.mongodb.net/expense?retryWrites=true&w=majority";
 
-const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log(`MongoDB connected on ${conn.connection.host}`);
-    } catch (err) {
-        console.log(err.message);
-        process.exit(1);
-    }
-}
+    console.log(`MongoDB connected on ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
